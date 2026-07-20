ecfdd216b921b14182354206d5276d5a33cbadc2
Zion Corbanie <zioncorb@gmail.com>
2026-07-20 10:16:47 +0200

    working with 0.55

--- scripts/on-monitor-change.sh
+++ scripts/on-monitor-change.sh
@@ -6,6 +6,6 @@ wallpaper="$HOME/Images/darkmogus.png"
 
 monitors=$(hyprctl monitors -j | jq -r '.[].name')
 for mon in $monitors; do
-  swww img "$wallpaper" --outputs "$mon" --transition-type any --transition-duration 1
+  awww img "$wallpaper" --outputs "$mon" --transition-type any --transition-duration 1
 done
 
